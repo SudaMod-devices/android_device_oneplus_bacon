075d742e03ac30f786eef08de53edb00a9b33f14
ghbhaha <ghbhaha@gmail.com>
2015-01-13 19:14:48 +0800

    update strings
    Change-Id: I9b7fa675aca832f283b875ab19d09ef74416c67a

--- sm.mk
+++ sm.mk
@@ -30,7 +30,7 @@ $(call inherit-product, vendor/sm/config/common_full_phone.mk)
 
 PRODUCT_NAME := sm_bacon
 PRODUCT_DEVICE := bacon
-PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MANUFACTURER := ONEPLUS
 PRODUCT_MODEL := A0001
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
